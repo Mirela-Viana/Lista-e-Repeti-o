03fb4e7231ae2f343cca054cbc82dd550a7a322d
unknown <MirelaViana.Melo@gmail.com>
2023-04-13 16:42:57 -0300

    (Refatoração do commit anterior) Realização da lista com 10 números + Soma dos números ao quadrado (números inseridos)

--- Exercício9.py
+++ Exercício9.py
@@ -1,9 +1,9 @@
-vetor_A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Vetor_A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 quadrados= []
 for i in range(10):
 
 
-    quadrados = sum([num**2 for num in vetor_A])
+    quadrados = sum([num**2 for num in Vetor_A])
 
-print("Vetor A:", vetor_A)
+print("Vetor A:", Vetor_A)
 print("Soma dos quadrados:", quadrados)
