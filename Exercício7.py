8e4c12fd9ef7f5238eeabda8f84e509863eac900
unknown <MirelaViana.Melo@gmail.com>
2023-04-13 15:04:50 -0300

    Refatoração do commit anterior, referente ao mesmo exercício =  Realização da lista de soma e multiplicação com 5 números inteiros.

--- Exercício7.py
+++ Exercício7.py
@@ -1,11 +1,11 @@
 ListaSOMA = [1, 2, 3, 4, 5]
 Soma= []
 Mult = []
-for num in ListaSOMA:
+for Num in ListaSOMA:
     Soma = sum(ListaSOMA)
     Mult = 1
-for num in ListaSOMA:
-     Mult = Mult * num
+for Num in ListaSOMA:
+     Mult = Mult * Num
 print("Números inteiros", ListaSOMA)
 print("Soma dos Números", Soma)
 print("Multiplicação dos Números", Mult)
