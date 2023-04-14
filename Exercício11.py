88a3706fa85ebabe3a6b65f8c72fd823a10477ae
unknown <MirelaViana.Melo@gmail.com>
2023-04-14 08:54:26 -0300

    Realização da segunda lista intercalada = 3 Listas com diferentes fatores sendo as duas primeiras listas com números inteiros e a terceira lista é referente a letras.

--- Exercício11.py
+++ Exercício11.py
@@ -1,13 +1,13 @@
-Lista_1 = [1,3,5,7,9,11,13,15,17,19]
-Lista_2 = [2,4,6,8,10,12,14,16,18,20]
-Lista_3 = ['A','B','C','D','E','F','G','H','I','J']
+lista_1 = [1,3,5,7,9,11,13,15,17,19]
+lista_2 = [2,4,6,8,10,12,14,16,18,20]
+lista_3 = ['A','B','C','D','E','F','G','H','I','J']
 intercalado= []
 for i in range(10):
-    intercalado.append(Lista_1[i])
-    intercalado.append(Lista_2[i])
-    intercalado.append(Lista_3[i])
+    intercalado.append(lista_1[i])
+    intercalado.append(lista_2[i])
+    intercalado.append(lista_3[i])
 
-print('Vetor A', Lista_1)
-print('Vetor B', Lista_2)
-print('Vetor C', Lista_3)
+print('Vetor A', lista_1)
+print('Vetor B', lista_2)
+print('Vetor C', lista_3)
 print('Lista Intercalada',intercalado)
